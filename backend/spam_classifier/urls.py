ee532f7b1df2c73d7ae1154add50cef56c782d23
Cesar Alejandro <154447276+AGUIRREMTZ@users.noreply.github.com>
2026-01-12 07:38:14 -0600

    Update urls.py

--- backend/spam_classifier/urls.py
+++ backend/spam_classifier/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('predict', views.predict, name='predict'),
     path('health', views.health, name='health'),
     path('info', views.model_info, name='model_info'),
+    path('metrics', views.model_metrics, name='model_metrics'),
 ]
